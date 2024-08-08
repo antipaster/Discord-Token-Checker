2e74c3e3fc77323f172bba1b85408ec01805cc95
god <121552016+antipaster@users.noreply.github.com>
2024-08-08 04:23:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,36 +15,44 @@ unclaimed_count = 0
 mail_verified_count = 0
 phone_verified_count = 0
 full_verified_count = 0
+billing_info_count = 0  
 
-results = []  
-BADGES = {
-    1 << 0: "DISCORD_EMPLOYEE",
-    1 << 1: "PARTNERED_SERVER_OWNER",
-    1 << 2: "HYPESQUAD_EVENTS",
-    1 << 3: "BUG_HUNTER_LEVEL_1",
-    1 << 4: "HOUSE_BRAVERY",
-    1 << 5: "HOUSE_BRILLIANCE",
-    1 << 6: "HOUSE_BALANCE",
-    1 << 7: "EARLY_SUPPORTER",
-    1 << 8: "TEAM_USER",
-    1 << 9: "BUG_HUNTER_LEVEL_2",
-    1 << 10: "VERIFIED_BOT",
-    1 << 11: "EARLY_VERIFIED_BOT_DEVELOPER",
-    1 << 12: "DISCORD_CERTIFIED_MODERATOR",
-    1 << 13: "DISCORD_PARTNER",
-    1 << 14: "VERIFIED_DEVELOPER",
+results = []
+gifts = []  
+promos = []  
 
-}
+proxies = []
+use_proxies = False
 
+def load_proxies():
+    global proxies
+    with open("proxies.txt", "r") as file:
+        proxies = [line.strip() for line in file.readlines()]
+        proxies = [f"http://{proxy}" if not proxy.startswith("http") else proxy for proxy in proxies]
 
+def get_random_proxy():
+    import random
+    return random.choice(proxies) if proxies else None
+
+def get_request_headers(token):
+    return {'Authorization': token}
+
+def get_request_proxies():
+    if use_proxies:
+        proxy = get_random_proxy()
+        if proxy:
+            return {
+                'http': proxy,
+                'https': proxy
+            }
+    return None
 
 def check_token_verification(token):
     global mail_verified_count, phone_verified_count, full_verified_count, unclaimed_count
-    headers = {
-        'Authorization': token
-    }
+    headers = get_request_headers(token)
+    proxy = get_request_proxies()
 
-    response = requests.get('https://discord.com/api/v10/users/@me', headers=headers)
+    response = requests.get('https://discord.com/api/v10/users/@me', headers=headers, proxies=proxy)
 
     if response.status_code == 200:
         data = response.json()
@@ -69,11 +77,10 @@ def check_token_verification(token):
         return "error"
 
 def check_boosts(token):
-    headers = {
-        'Authorization': token
-    }
+    headers = get_request_headers(token)
+    proxy = get_request_proxies()
 
-    response = requests.get('https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots', headers=headers)
+    response = requests.get('https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots', headers=headers, proxies=proxy)
     if response.status_code == 200:
         data = response.json()
         if data:  # Check if data is not empty
@@ -84,6 +91,45 @@ def check_boosts(token):
             return 0
     else:
         return 0
+
+def check_billing_info(token):
+    global billing_info_count
+    headers = get_request_headers(token)
+    proxy = get_request_proxies()
+
+    response = requests.get('https://discord.com/api/v10/users/@me/billing/payment-sources', headers=headers, proxies=proxy)
+    if response.status_code == 200:
+        data = response.json()
+        billing_info_count += len(data)
+        return len(data)
+    else:
+        return 0
+
+def check_gifts(token):
+    headers = get_request_headers(token)
+    proxy = get_request_proxies()
+
+    response = requests.get('https://discord.com/api/v10/users/@me/entitlements/gift-codes', headers=headers, proxies=proxy)
+    if response.status_code == 200:
+        data = response.json()
+        gift_codes = [gift['code'] for gift in data]
+        gifts.extend(gift_codes)
+        return gift_codes
+    else:
+        return []
+
+def check_promos(token):
+    headers = get_request_headers(token)
+    proxy = get_request_proxies()
+
+    response = requests.get('https://discord.com/api/v9/users/@me/outbound-promotions/codes?locale=fr', headers=headers, proxies=proxy)
+    if response.status_code == 200:
+        data = response.json()
+        promo_codes = [promo['code'] for promo in data]
+        promos.extend(promo_codes)
+        return promo_codes
+    else:
+        return []
 
 def save_tokens_to_file(tokens, filename):
     with open(filename, "w", encoding="utf-8") as file:
@@ -97,20 +143,13 @@ def extract_creation_year(user_id):
     creation_date = datetime.datetime.utcfromtimestamp(timestamp / 1000)
     return creation_date.year
 
-def get_badges(flags):
-    badge_list = [name for value, name in BADGES.items() if flags & value]
-    return ', '.join(badge_list) if badge_list else "No Badges"
-
-
-
 def check_token(token):
     global valid_tokens_count, invalid_tokens_count, nitro_count
 
-    headers = {
-        'Authorization': token
-    }
+    headers = get_request_headers(token)
+    proxy = get_request_proxies()
 
-    response = requests.get('https://discord.com/api/v9/users/@me', headers=headers)
+    response = requests.get('https://discord.com/api/v9/users/@me', headers=headers, proxies=proxy)
     if response.status_code == 200:
         valid_tokens_count += 1
         user_data = response.json()
@@ -118,17 +157,19 @@ def check_token(token):
         creation_year = extract_creation_year(user_id)
         premium_type = user_data.get('premium_type', 0)
         public_flags = user_data.get('public_flags', 0) 
-        badges = get_badges(public_flags)
         verification = check_token_verification(token)
         boosts = check_boosts(token)
+        billing_info = check_billing_info(token) 
+        gift_codes = check_gifts(token)  
+        promo_codes = check_promos(token)  
         nitro = "NITRO" if premium_type != 0 else "NO NITRO"
         if premium_type != 0:
             nitro_count += 1
-        result = f"{token} | {verification} | Creation Year: {creation_year} | Nitro: {nitro} | Boosts: {boosts} | Badges: {badges}"
+        result = f"{token} | {verification} | Creation Year: {creation_year} | Nitro: {nitro} | Boosts: {boosts} | Billing Info: {billing_info} | Gifts: {len(gift_codes)} | Promos: {len(promo_codes)}"
         results.append(result)
 
-        print(f'{lc} {Fore.LIGHTBLUE_EX}token={Fore.WHITE}{token[:20]}...{Fore.RESET} Flags: {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.GREEN}VALID{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET} {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}{boosts}_BOOSTS{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}{nitro}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}{verification}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}Creation Year: {creation_year}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}Badges: {badges}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}')
-        return token, verification, boosts, nitro, creation_year, badges
+        print(f'{lc} {Fore.LIGHTBLUE_EX}token={Fore.WHITE}{token[:20]}...{Fore.RESET} Flags: {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.GREEN}VALID{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET} {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}{boosts}_BOOSTS{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}{nitro}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}{verification}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}Creation Year: {creation_year}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}Billing Info: {billing_info}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}Gifts: {len(gift_codes)}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}, {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.BLUE}Promos: {len(promo_codes)}{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}')
+        return token, verification, boosts, nitro, creation_year, billing_info, gift_codes, promo_codes
     elif response.status_code == 401:
         invalid_tokens_count += 1
         print(f'{lc} {Fore.LIGHTBLUE_EX}token={Fore.WHITE}{token[:20]}...{Fore.RESET} Flags: {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.RED}INVALID{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}')
@@ -138,44 +179,70 @@ def check_token(token):
         time.sleep(retry_after)
         return check_token(token)
     else:
-        print(f'{lc} {Fore.LIGHTBLUE_EX}token={Fore.WHITE}{token[:20]}...{Fore.RESET} Flags: {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.RED}ERROR{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}')
-    return None
+        print(f'{lc} {Fore.LIGHTBLUE_EX}token={Fore.WHITE}{token[:20]}...{Fore.RESET} Flags: {Fore.RESET}{Fore.LIGHTBLACK_EX}{Style.BRIGHT}[{Fore.YELLOW}ERROR{Style.BRIGHT}{Fore.LIGHTBLACK_EX}]{Fore.RESET}')
+        return None
 
-def main():
-    os.system("Title Token checker discord.gg/nekito")
-    print(Fore.LIGHTMAGENTA_EX + '''
-    discord.gg/nekito (fuck yo ascii)
-    ''')
-    with open("tokens.txt", "r") as file:
-        tokens = file.readlines()
+print(Fore.GREEN + '''
+           /$$                                                   /$$                           /$$                
+          |__/                                                  | $$                          | $$                
+  /$$$$$$$ /$$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$         /$$$$$$$| $$$$$$$   /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$ 
+ /$$_____/| $$ /$$__  $$| $$_  $$_  $$ |____  $$       /$$_____/| $$__  $$ /$$__  $$ /$$_____/| $$  /$$/ |____  $$
+|  $$$$$$ | $$| $$  \ $$| $$ \ $$ \ $$  /$$$$$$$      | $$      | $$  \ $$| $$$$$$$$| $$      | $$$$$$/   /$$$$$$$
+ \____  $$| $$| $$  | $$| $$ | $$ | $$ /$$__  $$      | $$      | $$  | $$| $$_____/| $$      | $$_  $$  /$$__  $$
+ /$$$$$$$/| $$|  $$$$$$$| $$ | $$ | $$|  $$$$$$$      |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$| $$ \  $$|  $$$$$$$
+|_______/ |__/ \____  $$|__/ |__/ |__/ \_______/       \_______/|__/  |__/ \_______/ \_______/|__/  \__/ \_______/
+               /$$  \ $$                                                                                          
+              |  $$$$$$/                                                                                          
+               \______/                                                                                                                                                                  
+    discord.gg/nekito 
+    ''' + Fore.RESET)
 
-    tokens = [token.strip() for token in tokens]
-    num_threads = 3
-    total_tokens = len(tokens)
-    tokens_per_thread = total_tokens // num_threads
+use_proxies_input = input("Do you want to use proxies? (yes/no): ").strip().lower()
+use_proxies = use_proxies_input == "yes"
+num_threads = int(input("Threads: "))
+if use_proxies:
+    load_proxies()
 
-    def check_tokens_worker(start, end):
-        for i in range(start, end):
-            token = tokens[i]
-            result = check_token(token)
-        
-    threads = []
-    for i in range(num_threads):
-        start = i * tokens_per_thread
-        end = (i + 1) * tokens_per_thread if i < num_threads - 1 else total_tokens
-        thread = threading.Thread(target=check_tokens_worker, args=(start, end))
-        thread.start()
-        threads.append(thread)
+with open("tokens.txt", "r") as file:
+    tokens = file.readlines()
 
-    for thread in threads:
-        thread.join()
+tokens = [token.strip() for token in tokens]
 
-    # Save results to results.txt
-    with open("results.txt", "w", encoding="utf-8") as file:
-        for result in results:
-            file.write(f"{result}\n")
+total_tokens = len(tokens)
+tokens_per_thread = total_tokens // num_threads
 
-    print(f"{lc}{Fore.GREEN} {'Valid Tokens: ' + str(valid_tokens_count) + f' {Fore.RESET}|{Fore.GREEN} ' if valid_tokens_count > 0 else ''}{f'{Fore.RED}Invalid Tokens: ' + str(invalid_tokens_count) + f' {Fore.RESET}|{Fore.GREEN} ' if invalid_tokens_count > 0 else ''}{'Nitro: ' + str(nitro_count) + f' {Fore.RESET}|{Fore.GREEN} ' if nitro_count > 0 else ''}{'Unclaimed: ' + str(unclaimed_count) + f' {Fore.RESET}|{Fore.GREEN} ' if unclaimed_count > 0 else ''}{'Mail Verified: ' + str(mail_verified_count) + f' {Fore.RESET}|{Fore.GREEN} ' if mail_verified_count > 0 else ''}{'Phone Verified: ' + str(phone_verified_count) + f' {Fore.RESET}|{Fore.GREEN} ' if phone_verified_count > 0 else ''}{'Full Verified: ' + str(full_verified_count) if full_verified_count > 0 else ''}")
+def check_tokens_worker(start, end):
+    for i in range(start, end):
+        token = tokens[i]
+        result = check_token(token)
+
+threads = []
+for i in range(num_threads):
+    start = i * tokens_per_thread
+    end = (i + 1) * tokens_per_thread if i < num_threads - 1 else total_tokens
+    thread = threading.Thread(target=check_tokens_worker, args=(start, end))
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+
+
+with open("results.txt", "w", encoding="utf-8") as file:
+    for result in results:
+        file.write(f"{result}\n")
+
+
+with open("gifts.txt", "w", encoding="utf-8") as file:
+    for gift in gifts:
+        file.write(f"{gift}\n")
+
+
+with open("promos.txt", "w", encoding="utf-8") as file:
+    for promo in promos:
+        file.write(f"{promo}\n")
+
+print(f"{lc}{Fore.GREEN} {'Valid Tokens: ' + str(valid_tokens_count) + f' {Fore.RESET}|{Fore.GREEN} ' if valid_tokens_count > 0 else ''}{f'{Fore.RED}Invalid Tokens: ' + str(invalid_tokens_count) + f' {Fore.RESET}|{Fore.GREEN} ' if invalid_tokens_count > 0 else ''}{'Nitro: ' + str(nitro_count) + f' {Fore.RESET}|{Fore.GREEN} ' if nitro_count > 0 else ''}{'Unclaimed: ' + str(unclaimed_count) + f' {Fore.RESET}|{Fore.GREEN} ' if unclaimed_count > 0 else ''}{'Mail Verified: ' + str(mail_verified_count) + f' {Fore.RESET}|{Fore.GREEN} ' if mail_verified_count > 0 else ''}{'Phone Verified: ' + str(phone_verified_count) + f' {Fore.RESET}|{Fore.GREEN} ' if phone_verified_count > 0 else ''}{'Full Verified: ' + str(full_verified_count) + f' {Fore.RESET}|{Fore.GREEN} ' if full_verified_count > 0 else ''}{'Billing Info: ' + str(billing_info_count) if billing_info_count > 0 else ''}")
 
 main()
 input("")
